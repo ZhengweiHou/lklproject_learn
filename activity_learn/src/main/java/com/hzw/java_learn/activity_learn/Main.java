450018c059fb8367fc8b98ffe0d5731d5771897e
侯正伟 <houzw@allinfinance.com>
2017-04-18 18:05:07 +0800

    activiti + baseDao

---
+++ activity_learn/src/main/java/com/hzw/java_learn/activity_learn/Main.java
@@ -0,0 +1,14 @@
+package com.hzw.java_learn.activity_learn;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+
+public class Main {
+	public static void main(String[] args) {
+		ApplicationContext  context = new ClassPathXmlApplicationContext("spring-context.xml");
+		
+		System.out.println("asjfklashf");
+	}
+}

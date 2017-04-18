9ea915e83a1ec504a8f578069a63a47d40304220
侯正伟 <houzw@allinfinance.com>
2017-04-18 14:55:13 +0800

    mavenPlugin测试代码

---
+++ mavenplugintest/src/main/java/org/mavenplugintest/App.java
@@ -0,0 +1,13 @@
+package org.mavenplugintest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

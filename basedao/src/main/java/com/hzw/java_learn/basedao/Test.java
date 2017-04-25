e6e039faf9fbe4f3b3a88ae1164a0b3ea0ce9bc6
侯正伟 <houzw@allinfinance.com>
2017-04-25 14:56:25 +0800

    添加dubbo项目

---
+++ basedao/src/main/java/com/hzw/java_learn/basedao/Test.java
@@ -0,0 +1,10 @@
+package com.hzw.java_learn.basedao;
+
+public class Test {
+	public static void main(String[] args) {
+		
+		String a = null;
+		
+		System.out.println("asfd".equals(a));
+	}
+}
---
+++ dubbo_learn/src/main/java/org/dubbo_learn/App.java
@@ -0,0 +1,13 @@
+package org.dubbo_learn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

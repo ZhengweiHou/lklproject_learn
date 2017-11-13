2504002736ff864605f5cd61000d81d210c899f1
侯正伟 <houzw@allinfinance.com>
2017-11-13 18:10:32 +0800

    JAVA基础  - 泛型1

---
+++ javafoundation/src/main/java/com/hzw/java_learn/javafoundation/generic/Learn1.java
@@ -0,0 +1,81 @@
+package com.hzw.java_learn.javafoundation.generic;
+
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Learn1 {
+	public static void main(String[] args) {
+		
+		
+		AInt<String> a1 = new AImpl<Integer>();
+		a1.showClass("String");
+		a1.showClass();
+		
+	}
+}
+
+/**
+ * 接口
+ * @author houzw
+ * @param <T>
+ */
+interface AInt<T>{
+	public void showClass(T entity);
+	public void showClass();
+}
+
+
+
+
+/**
+ * 父类
+ * @author houzw
+ * @param <G>
+ */
+abstract class APar<G>{
+	public void parShowclass(){
+		Class c = this.getClass();
+		Type t = c.getGenericSuperclass(); // 获取继承父类中的泛型类型,子类继承调用
+		System.out.println(t);
+		if (t instanceof ParameterizedType) {
+			Type[] p = ((ParameterizedType) t).getActualTypeArguments();
+			System.out.println(Arrays.toString(p));
+        }
+	}
+}
+
+/**
+ * 接口的实现类
+ * @author houzw
+ * @param <E>
+ */
+class AImpl<E> extends APar<Integer> implements AInt<String>{
+	
+	public AImpl(){
+		Class c = this.getClass();
+		
+		Type[] tl = c.getGenericInterfaces(); // 获取实现的接口中的泛型类型数组
+		Type t = tl[0];
+		System.out.println(t);
+		if (t instanceof ParameterizedType) {
+			Type[] p = ((ParameterizedType) t).getActualTypeArguments();
+			System.out.println(Arrays.toString(p));
+        }
+		
+	}
+
+	// 实现接口时指定了接口的泛型类型,则接口中的所有泛型参数全部被替换	String => T	则 showClass(String entity) => showClass(T entity)
+	public void showClass(String entity) {
+		// TODO Auto-generated method stub
+	}
+
+	public void showClass() {
+		 this.parShowclass(); // 调用父类的方法
+		
+	}
+	
+	
+	
+}
